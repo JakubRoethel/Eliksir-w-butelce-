a8a930931155cd717dbe2532af1c64cc24147868
Błaej Szafrański <szafranski96@gmail.com>
2023-03-25 12:28:36 +0100

    Hover call to action changes

--- views/home-call-to-action.php
+++ views/home-call-to-action.php
@@ -3,13 +3,13 @@
           if( $call_to_action_list): ?>
           <div class="call_to_action_container">
           <?php  foreach( $call_to_action_list as $call_to_action_list) { 
-                         $img = $call_to_action_list['img'];
+                         $img_url = $call_to_action_list['img'];
                          $title = $call_to_action_list['title'];
                          $description = $call_to_action_list['text'];
                     ?>
-                        <div class="single_call_to_action">
+                        <div class="single_call_to_action" style="background-image: url(<?php echo $img_url ?>)">
                             <div class="mask"></div>
-                            <?php echo wp_get_attachment_image( $img, 'full' ); ?>
+                           
                             <h2 class="title"> <?php echo $title  ?> </h2>
                             <p class="description">  <?php echo $description  ?> </p>
                         </div>
