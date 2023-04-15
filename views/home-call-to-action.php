97cc55d9697648c4d52e414f86fe41933196bec7
404Studio <roethel.jakub@gmail.com>
2023-04-15 14:01:22 +0200

    add to cart button css fix

--- views/home-call-to-action.php
+++ views/home-call-to-action.php
@@ -8,13 +8,14 @@
                          $title = $call_to_action_list['title'];
                          $description = $call_to_action_list['text'];
                          $text_on_hover = $call_to_action_list['text_on_hover'];
+                         $link = $call_to_action_list['link'];
                     ?>
-                        <div class="single_call_to_action" style="background-image: url(<?php echo $img_url ?>)">
+                        <a href="<?php echo $link ?>" class="single_call_to_action" style="background-image: url(<?php echo $img_url ?>)">
                             <div class="mask"></div>
                             <h2 class="title"> <?php echo $title  ?> </h2>
                             <p class="description">  <?php echo $description  ?> </p>
                             <p class="hover_text"> <?php echo $text_on_hover  ?> </p>
-                        </div>
+                        </a>
                       <?php  } ?>
           </div>
       <?php endif; ?> 
